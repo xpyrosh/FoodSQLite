939d14f92ee80d2f724cc9666ffb89fc9f36cbd7
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-08-27 01:14:21 -0400

    Initial commit
    First commit of learning android
    
    Created layout of buttons and text fields, as well as methods for the SQLiteHelper class to deal with DB

---
+++ app/src/main/java/com/example/foodsqlite/SQLiteHelper.java
@@ -0,0 +1,51 @@
+package com.example.foodsqlite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    //Constructor autocreated
+    public SQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //Method for querying data
+    public void queryData(String sql){
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    //Method for data insertion to database
+    public void insertData(String name, String price, byte[] image){
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO FOOD VALUES (NULL, ?, ?, ?)";
+
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+
+        statement.bindString(1, name);
+        statement.bindString(2, price);
+        statement.bindBlob(3, image);
+
+        statement.executeInsert();
+    }
+
+    //Method for getting data back from DB
+    public Cursor getData(String sql){
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery(sql, null);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
