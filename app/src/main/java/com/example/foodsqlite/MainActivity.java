ae9eced7ac22daa5c20907bd1510307a7dd0d9aa
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-08-27 01:51:14 -0400

    Followed tutorial to add click listener to the add image button and handle all exceptions dealing with accessing internal storage. However old tutorial works at this point whereas mine suddenly crashes and the app won't work.

--- app/src/main/java/com/example/foodsqlite/MainActivity.java
+++ app/src/main/java/com/example/foodsqlite/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.foodsqlite;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editName, editPrice;
+    Button btnAdd, btnChoose, btnFoodList;
+    ImageView imageView;
+
+    final int REQUEST_CODE_GALLERY = 999;
+
+    public static SQLiteHelper sqLiteHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        sqLiteHelper = new SQLiteHelper(this, "FoodDB.sqlite", null, 1);
+
+        sqLiteHelper.queryData("CREATE TABLE IF NOT EXISTS FOOD (Id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, price VARCHAR, image BLOG");
+
+        //Attaching click listener to the choose image button
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "You don't have access to these files", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void init(){
+        //Connecting to parts on view
+        editName = (EditText) findViewById(R.id.editName);
+        editPrice = (EditText) findViewById(R.id.editPrice);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnChoose = (Button) findViewById(R.id.btnChoose);
+        btnFoodList = (Button) findViewById(R.id.btnFoodList);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 }
